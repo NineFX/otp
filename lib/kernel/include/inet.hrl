fdf25edb98daefaf0397454414ad0aefecfd8762
Drew Varner <drew.varner@ninefx.com>
2021-11-17 08:15:09 -0500

    Correct hostent_addr h_addrtype typespec

--- lib/kernel/include/inet.hrl
+++ lib/kernel/include/inet.hrl
@@ -1,8 +1,8 @@
 %%
 %% %CopyrightBegin%
-%% 
+%%
 %% Copyright Ericsson AB 1997-2017. All Rights Reserved.
-%% 
+%%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
@@ -14,11 +14,14 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
-%% 
+%%
 %% %CopyrightEnd%
 %%
 %% This record is returned by inet:gethostbyaddr/2 and inet:gethostbyname/2.
 
+-type hostent_addrtype() :: inet | inet6 | caa | cname | gid | hinfo | ns | mb
+                          | md | mg | mf | minfo | mx | naptr | null | ptr | soa
+                          | spf | srv | txt | uid | uinfo | unspec | uri | wks.
 
 -record(hostent,
 	{
